372620b8ab45978d779f589ef68ef2925178c291
gogo <hitmangogo7@gmail.com>
2025-02-12 15:30:54 +0530

    fix img issue

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -35,9 +35,9 @@ export const BentoGridItem = ({
   header?: React.ReactNode;
   icon?: React.ReactNode;
   id: number;
-  img: string
-  imgClassName: string
-  titleClassName: string
+  img: string;
+  imgClassName: string;
+  titleClassName: string;
   spareImg: string
 }) => {
   return (
@@ -49,9 +49,21 @@ export const BentoGridItem = ({
 
       style={{
         background: `rgb(4,7,29)`,
-        backgroundColor: 'gradient...'
+        backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)'
       }}
     >
+      <div className={`${id === 6} && flex justify-center h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+             <img 
+              src={img}
+              alt={img}
+              className={cn(imgClassName, 'object-cover', 'object-center')}
+              />
+            )} 
+
+        </div>
+      </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
